51380eb3bb00b64971f4907a7203d6e3961e0c1e
littleapple27 <littleapple27dev@gmail.com>
2022-07-27 14:13:58 -0400

    add more cypress tests for parallel run set up

--- cypress/integration/no-js-form/no-js-form.spec.js
+++ cypress/integration/no-js-form/no-js-form.spec.js
@@ -1,11 +1,27 @@
 /// <reference types="cypress" />
 
-describe("example index page tests", () => {
+const getInputByLabel = (label) => {
+  return cy
+    .contains("label", label)
+    .invoke("attr", "for")
+    .then((id) => {
+      cy.get("#" + id);
+    });
+};
+
+describe("example no-js-form tests", () => {
   beforeEach(() => {
     cy.visit("/no-js-form");
   });
 
-  it("tests next form with no js", () => {
-    cy.log('here you go')
+  it("tests no-js-form page render", () => {
+    cy.get("h1").should("exist").and("have.text", "Form without JavaScript.");
+  });
+
+  it("tests js-form inputs and submit", () => {
+    getInputByLabel("First Name").type("Andrea");
+    getInputByLabel("Last Name").type("Legleiter");
+    cy.get("button").should("have.text", "Submit").click({ force: true });
+    cy.url().should("be.equal", "http://localhost:3000/api/form");
   });
 });
