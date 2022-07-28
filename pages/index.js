591344f6c379db06848e39ee0f891162b8a13b3e
littleapple27 <38861849+littleapple27@users.noreply.github.com>
2022-07-28 13:39:50 -0400

    testing request review workflow (#7)
    * testing request review workflow
    
    * update labelWhenApproved
    
    * add automerge
    
    * attempt

--- pages/index.js
+++ pages/index.js
@@ -42,8 +42,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer">
-          Built with Next.js | Powered by | Tested by littleapple27 | CI/CD
-          Example
+          Built with Next.js | Powered by {""}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
